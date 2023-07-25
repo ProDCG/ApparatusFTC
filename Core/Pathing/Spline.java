1f0aaa5e849944f30c838642c7583fe7dbf715c2
ProDCG <mason.stuart05@outlook.com>
2023-07-25 13:48:36 -0700

    Initial commit, has pathing capabilities

---
+++ Core/Pathing/Spline.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Spline {
+    private Polynomial x, y;
+
+    public Spline(Polynomial x, Polynomial y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point calculate(double t) {
+        return new Point(x.calculate(t), y.calculate(t));
+    }
+}
---
+++ Core/Pathing/Vector2D.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class Vector2D {
+    public double x,y;
+
+    public Vector2D(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public static Vector2D fromHeadingAndMagnitude(double h, double m){
+        return new Vector2D(Math.cos(h) * m, Math.sin(h) * m);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%.2f, %.2f)", x, y);
+    }
+}
